07e499cb687eb3ad469516e8646470e42ef0cb40
Kirill Makarov <ikir2826@gmail.com>
2023-12-14 18:25:42 +0300

    Валидация регистрации

--- src/Pages/Registration.tsx
+++ src/Pages/Registration.tsx
@@ -1,7 +1,49 @@
+import { useState } from 'react';
+
+interface Fields {
+  login: string;
+  password: string;
+  secondPassword: string;
+}
+
+interface Errors {
+  invalidLogin: boolean | undefined;
+  invalidPassword: boolean | undefined;
+  notEqualPasswords: boolean | undefined;
+}
+
 function Registration() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement> | undefined) => {
-    console.log(e);
+  const [errors, setErrors] = useState<Errors>({
+    invalidLogin: false,
+    invalidPassword: false,
+    notEqualPasswords: false,
+  });
+
+  const handleValidation = (fields: Fields) => {
+    const tempErrors: Errors = {
+      invalidLogin: fields.login.length <= 6 ? true : false,
+      invalidPassword: fields.password.length <= 6 ? true : false,
+      notEqualPasswords: fields.password !== fields.secondPassword ? true : false,
+    };
+    Object.values(tempErrors).some((error) => error) ? console.log('not ok') : console.log('ok');
+    setErrors(tempErrors);
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
     e?.preventDefault();
+    const targetFields = e.target as typeof e.target & {
+      login: { value: string };
+      password: { value: string };
+      secondPassword: { value: string };
+    };
+
+    const fields = {
+      login: targetFields.login.value,
+      password: targetFields.password.value,
+      secondPassword: targetFields.secondPassword.value,
+    };
+
+    handleValidation(fields);
   };
 
   return (
@@ -9,11 +51,14 @@ function Registration() {
       <h1 className='form-title'>Регистрация</h1>
       <form className='form' onSubmit={handleSubmit}>
         <h3>Введите логин</h3>
-        <input className='form-input' type='text' />
+        <input className='form-input' type='text' name='login' />
+        {errors.invalidLogin && <span>Ошибка</span>}
         <h3>Введите пароль</h3>
-        <input className='form-input' type='password' />
+        <input className='form-input' type='password' name='password' />
+        {errors.invalidPassword && <span>Ошибка</span>}
         <h3>Повторите пароль</h3>
-        <input className='form-input' type='password' />
+        <input className='form-input' type='password' name='secondPassword' />
+        {errors.notEqualPasswords && <span>Ошибка</span>}
         <button className='form-button' type='submit'>
           Зарегистрироваться
         </button>
