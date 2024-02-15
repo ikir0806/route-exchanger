7710439bf06182459686bc8e010ab2ec54f3af24
Kirill Makarov <ikir2826@gmail.com>
2024-02-15 17:10:54 +0300

    Popup, часть маркера

--- src/Pages/Constructor.tsx
+++ src/Pages/Constructor.tsx
@@ -1,7 +1,11 @@
-import { Map, View } from 'ol';
+import { Map, Overlay, View } from 'ol';
 import apply from 'ol-mapbox-style';
+import Draw from 'ol/interaction/Draw.js';
+import Interaction from 'ol/interaction/Interaction';
 import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
 import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
 import { useEffect, useRef } from 'react';
 // import './styles.css';
 
@@ -12,19 +16,72 @@ function MapView({ zoom = 1 }: { zoom?: number }) {
   const mapStyle = 'https://maps.geoapify.com/v1/styles/positron/style.json';
   useEffect(() => {
     if (ref.current && !mapRef.current) {
+      const container = document.getElementById('popup') || undefined;
+      const content = document.getElementById('popup-content');
+      const closer = document.getElementById('popup-closer');
+
+      const raster = new TileLayer({
+        source: new OSM(),
+      });
+
+      const source = new VectorSource({ wrapX: false });
+      const vector = new VectorLayer({
+        source: source,
+        style: {
+          'fill-color': 'rgba(255, 255, 255, 0.2)',
+          'stroke-color': '#ffcc33',
+          'stroke-width': 2,
+          'circle-radius': 7,
+          'circle-fill-color': '#ffcc33',
+        },
+      });
+
+      const overlay = new Overlay({
+        element: container,
+        autoPan: {
+          animation: {
+            duration: 250,
+          },
+        },
+      });
+
+      if (closer) {
+        closer.onclick = function () {
+          overlay.setPosition(undefined);
+          closer.blur();
+          return false;
+        };
+      }
+
       mapRef.current = new Map({
-        layers: [new TileLayer({ source: new OSM() })],
+        layers: [raster, vector],
         view: new View({ center: [0, 0], zoom: 1 }),
         target: ref.current,
-        overlays: [
-          // {
-          //   position: fromLonLat([16.3725, 48.208889]),
-          //   positioning: 'bottom-right',
-          //   element: document.getElementById('marker'),
-          //   stopEvent: false,
-          // },
-        ],
+        overlays: [overlay],
       });
+
+      mapRef.current.on('singleclick', (e) => {
+        console.log(e);
+        const coordinate = e.coordinate;
+        const hdms = toStringHDMS(toLonLat(coordinate));
+        if (content) content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+        overlay.setPosition(coordinate);
+      });
+
+      const typeSelect = 'Point';
+
+      let draw: null | Draw | Interaction = null; // global so we can remove it later
+
+      const addInteraction = () => {
+        draw = new Draw({
+          source: source,
+          type: typeSelect,
+        });
+        mapRef.current?.addInteraction(draw);
+      };
+
+      addInteraction();
+
       apply(mapRef.current, `${mapStyle}?apiKey=${myAPIKey}`);
     }
   }, [ref, mapRef]);
@@ -40,6 +97,12 @@ export default function Constructor() {
   return (
     <div className='map-container'>
       <MapView zoom={2} />
+      <div id='popup' className='ol-popup'>
+        <a href='#' id='popup-closer' className='ol-popup-closer'>
+          X
+        </a>
+        <div id='popup-content'></div>
+      </div>
     </div>
   );
 }
