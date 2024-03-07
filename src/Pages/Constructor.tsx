c1e6b1a6572e2b496b39b4325d369b90528837b2
Kirill Makarov <ikir2826@gmail.com>
2024-03-07 15:46:25 +0300

    стилизация попапов маркеров

--- src/Pages/Constructor.tsx
+++ src/Pages/Constructor.tsx
@@ -185,15 +185,17 @@ export default function Constructor() {
         <a href='#' id='popup-closer' className='ol-popup-closer'>
           X
         </a>
-        <div id='popup-content'></div>
-        <input className='popup-input' id='popup-input' />
-        <button id='popup-confirm' className='ol-popup-confirm'>
-          Добавить
-        </button>
-
-        <button id='popup-cancel' className='ol-popup-cancel'>
-          Отмена
-        </button>
+        <div id='popup-content' className='ol-popup-content'></div>
+        <input className='ol-popup-input' id='popup-input' />
+        <br />
+        <div className='ol-popup-wrp'>
+          <button id='popup-confirm' className='ol-popup-button ol-popup-confirm'>
+            Добавить
+          </button>
+          <button id='popup-cancel' className='ol-popup-button ol-popup-cancel'>
+            Отмена
+          </button>
+        </div>
       </div>
       <div id='marker-popup' className='ol-popup'>
         <a href='#' id='marker-popup-closer' className='ol-popup-closer'>
